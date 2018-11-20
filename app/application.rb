6f962d193e6f893e47e21b1ae87075136dade055
Harley Rayner <harleymteden@gmail.com>
2018-11-20 00:32:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,7 @@ class Application
 
     elsif req.path.match(/add/)
         search_term = req.params["q"]
+        binding.pry
           if @@items.include?(search_term)
             resp.write "added #{search_term}"
             @@cart << "#{search_term}"
