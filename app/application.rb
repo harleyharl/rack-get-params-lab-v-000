ede882797d2ba66a6ff651b7f1b50c0540f726ca
Harley Rayner <harleymteden@gmail.com>
2018-11-20 00:16:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,8 @@ class Application
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      resp.write
+      handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
